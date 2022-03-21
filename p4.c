800c5a18761599a162a827c9be35a7086033b96e
MontseGDC <78836115+MontseGDC@users.noreply.github.com>
2022-03-20 19:57:19 -0600

    p4.c
    va

---
+++ p4.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+
+void Pedir_Valores (int *d, int *m, int *a);
+void Calcular_Dia( int d, int m, int a);
+
+int main(void)
+{
+ int d, m, a;
+ Pedir_Valores (&d, &m, &a);
+ Calcular_Dia(d, m, a);
+
+
+  return 0;
+}
+
+void  Pedir_Valores (int *d, int *m, int *a)
+{
+        printf ("\n Dame el dia: \n");
+        scanf ("%d", d );
+        printf("\n Dame el valor numerico del mes: \n");
+        scanf("%d",m);
+        printf("\n Dame el ano \n");
+        scanf("%d", a);
+
+}
+void Calcular_Dia( int d, int m, int a)
+{
+  int n;
+ if  (m == 1 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+ else {
+   printf("El dia no es valido, adios \n");
+ }
+ if  (m == 2 && d>0 && d<29  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+ if  (m == 3 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+ printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+   if  (m == 4 && d>0 && d<31  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+  if  (m == 5 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+
+if  (m == 6 && d>0 && d<31  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+ if  (m == 7 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+
+ if  (m == 8 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+ if  (m == 9 && d>0 && d<31  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+  if  (m == 10 && d>0 && d<32 ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+
+if  (m == 11 && d>0 && d<31  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ {
+  else if  (m == 12 && d>0 && d<32  ) {
+    printf("\n El dia anterior es\n" );
+    n = d - 1;
+    printf("El mes es:%d \n", m);
+    printf("El dia es: %d\n",n);
+    printf("El ano es: %d\n", a);
+ }
+else {
+   printf("El dia no es valido, adios \n");
+ }
+
+}
